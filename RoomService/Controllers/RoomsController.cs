bae71217e0b73f5de30ff9ebc5fbae0fce5a9b0e
Silpa2820 <silpapriyareddy@gmail.com>
2022-05-23 19:47:44 +0530

    first commit

---
+++ RoomService/Controllers/RoomsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RoomService.Models;
+using RoomService.Repository;
+
+namespace RoomService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomsController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            List<Room> rooms = RoomRepository.GetRooms();
+
+
+            return Ok(rooms);
+        }
+    }
+}
